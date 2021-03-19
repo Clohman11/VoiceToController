5cf4e7d14370577f7f1c50f06744909e561f565c
Clohman11 <colinlohman@gmail.com>
2021-03-19 14:18:58 -0400

    Probably final file setup

---
+++ Controller.py
@@ -0,0 +1,25 @@
+import pyvjoy
+import js2py #https://github.com/PiotrDabkowski/Js2Py
+#use vJoy
+
+class Controller:
+#Setup the controller in vJoy
+    def __init__():
+        p1 = setup(1)
+
+    def setup(Num):
+        done = False
+        while done = False:
+            try:
+                Controller = pyvjoy.VJoyDevice(Num)
+                done = True
+            except vJoyException:
+                Num = Num+1
+            if (Num >= 4):
+                print("No Avaliable VJoy Devices")
+                break
+
+    def reset():
+        p1.reset()
+
+    p1.set_button(2,1)
---
+++ MainThing.py
@@ -0,0 +1,17 @@
+import Controller.py
+import VoiceHandling.js
+import Authentication.js
+
+class V2Input:
+    def __init__():
+        self.Control = Controller()
+        self.VoiceObject = ??#get voice stuff
+
+    #Turn a voice command into a single button press
+    def Single_Button():
+
+    #Turn a voice command into a series of inputs
+    def Multi_Button():
+
+    #resets in case of mess-up situation
+    def Reset():
